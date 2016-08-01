9ec0b6cba779b0abec6774ecf171b7645e63585d
huangjiong <huangjiong@gomo.com>
2016-08-01 16:31:06 +0800

    修复value为空的逻辑判断

--- src/com/nfwork/dbfound/model/bean/Param.java
+++ src/com/nfwork/dbfound/model/bean/Param.java
@@ -6,9 +6,9 @@ import java.util.Date;
 import com.nfwork.dbfound.model.base.Entity;
 
 public class Param extends Entity {
-	
+
 	private static final long serialVersionUID = 5538229252299018282L;
-	
+
 	private String name;
 	private String dataType = "varchar";
 	private String ioType = "in";
@@ -18,19 +18,19 @@ public class Param extends Entity {
 	private String sourcePath;
 	private String sourcePathHistory;
 	private String scope;
-	private String fileSaveType = "db";  //disk ,db
+	private String fileSaveType = "db"; // disk ,db
 	private String fileNameParam;
 	private String UUID = "false";
 
 	@Override
-	public void run(){
+	public void run() {
 		if (getParent() instanceof Model) {
 			Model model = (Model) getParent();
 			model.getParams().put(name, this);
-		}else if (getParent() instanceof Execute) {
+		} else if (getParent() instanceof Execute) {
 			Execute execute = (Execute) getParent();
 			execute.getParams().put(name, this);
-		}else if (getParent() instanceof Query) {
+		} else if (getParent() instanceof Query) {
 			Query query = (Query) getParent();
 			query.getParams().put(name, this);
 		}
@@ -73,20 +73,22 @@ public class Param extends Entity {
 	}
 
 	public String getStringValue() {
-		if (value instanceof Date) {
-			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			return format.format(value);
+		if (value == null) {
+			return null;
+		} else {
+			if (value instanceof Date) {
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				return format.format(value);
+			} else {
+				return value.toString();
+			}
 		}
-		if (value != null) {
-			return value.toString();
-		}
-		return null;
 	}
 
 	public void setValue(String value) {
 		this.value = value;
 	}
-	
+
 	public void setValue(Object value) {
 		this.value = value;
 	}
@@ -146,5 +148,5 @@ public class Param extends Entity {
 	public void setAutoCookie(String autoCookie) {
 		this.autoCookie = autoCookie;
 	}
-	
+
 }
