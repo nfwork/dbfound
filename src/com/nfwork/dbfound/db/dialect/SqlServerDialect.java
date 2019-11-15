66a6cfe76cd15d1961ac56cb398ef68ecd07db4c
nfwork <nfwork@163.com>
2019-11-15 15:15:58 +0800

    修改sqlserver方言问题

--- src/com/nfwork/dbfound/db/dialect/SqlServerDialect.java
+++ src/com/nfwork/dbfound/db/dialect/SqlServerDialect.java
@@ -3,13 +3,17 @@ package com.nfwork.dbfound.db.dialect;
 public class SqlServerDialect implements SqlDialect {
 	public String getPagerSql(String sql, int pagerSize, long startWith) {
 
-		int index = sql.toLowerCase().indexOf("select");
-		sql = sql.substring(0, index+6)+" row_number()over(order by getdate()) d_p_rm ,"+sql.substring(index+6);
-		
-		String pagersql = " select * from (" + sql
-		+ ") v where d_p_rm <=" + (startWith + pagerSize)
-		+ " and d_p_rm >= " + (startWith + 1);
-		
+		int i = sql.toLowerCase().lastIndexOf("order by");
+		if (i == -1) {
+			i = sql.toLowerCase().lastIndexOf("ORDER BY");
+		}
+		String esql = i == -1 ? sql : sql.substring(0, i);
+		String orderBy = i == -1 ? "getdate()" : sql.substring(i + 9);
+
+		int index = esql.toLowerCase().indexOf("select");
+		esql = esql.substring(0, index + 6) + " row_number() over(order by " + orderBy + ") d_p_rm ," + esql.substring(index + 6);
+
+		String pagersql = " select * from (" + esql + ") v where d_p_rm <=" + (startWith + pagerSize) + " and d_p_rm >= " + (startWith + 1);
 		return pagersql;
 	}
 
