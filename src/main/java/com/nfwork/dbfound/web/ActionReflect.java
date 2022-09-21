ee55254ca2be912fd2dd8ec44070df5445c6a096
huangjiong <huangjiong@gomo.com>
2022-09-21 18:05:13 +0800

    修复Action异常处理逻辑

--- src/main/java/com/nfwork/dbfound/web/ActionReflect.java
+++ src/main/java/com/nfwork/dbfound/web/ActionReflect.java
@@ -1,5 +1,6 @@
 package com.nfwork.dbfound.web;
 
+import com.nfwork.dbfound.exception.DBFoundPackageException;
 import org.apache.commons.beanutils.MethodUtils;
 
 import com.nfwork.dbfound.core.Context;
@@ -9,29 +10,43 @@ import com.nfwork.dbfound.dto.ResponseObject;
 import com.nfwork.dbfound.exception.DBFoundRuntimeException;
 import com.nfwork.dbfound.web.base.BaseControl;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * 反射机制，动态的调用方法
  */
 public class ActionReflect {
 
-	public static void reflect(Context context, String className, String method, boolean singleton) throws Exception {
+	public static void reflect(Context context, String className, String method, boolean singleton) {
 		
 		//开启事务
 		Transaction transaction = context.getTransaction();
 		transaction.begin();
-		
-		BaseControl baseControl = ActionBeanFactory.getControl(className,singleton);
-		Object result = MethodUtils.invokeMethod(baseControl, method, new Object[] { context });
-		
-		if (result != null) {
-			if (result instanceof ResponseObject) {
-				// 提交关闭事务
-				transaction.commit();
-				transaction.end();
-				
-				WebWriter.jsonWriter(context.response, JsonUtil.beanToJson(result, context));
-			} else {
-				throw new DBFoundRuntimeException("return object must extends ResponseObject");
+
+		try {
+			BaseControl baseControl = ActionBeanFactory.getControl(className, singleton);
+			Object result = MethodUtils.invokeMethod(baseControl, method, new Object[]{context});
+
+			if (result != null) {
+				if (result instanceof ResponseObject) {
+					// 提交关闭事务
+					transaction.commit();
+					transaction.end();
+
+					WebWriter.jsonWriter(context.response, JsonUtil.beanToJson(result, context));
+				} else {
+					throw new DBFoundRuntimeException("return object must extends ResponseObject");
+				}
+			}
+		}catch (Exception e){
+			Throwable throwable = e.getCause();
+			if(throwable instanceof DBFoundRuntimeException){
+				throw (DBFoundRuntimeException)throwable;
+			}
+			if(e instanceof InvocationTargetException && throwable !=null){
+				throw new DBFoundPackageException("ActionReflect execute failed, "+e.getMessage(), (Exception) throwable);
+			}else{
+				throw new DBFoundPackageException("ActionReflect execute failed, "+e.getMessage(), e);
 			}
 		}
 	}
