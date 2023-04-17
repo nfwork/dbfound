a2fcdea78e508d6abf89e26e1353d2f10059ff31
huangjiong <huangjiong@gomo.com>
2023-04-17 18:48:43 +0800

    添加responseObject快速获取属性方法

--- src/main/java/com/nfwork/dbfound/dto/QueryResponseObject.java
+++ src/main/java/com/nfwork/dbfound/dto/QueryResponseObject.java
@@ -1,5 +1,6 @@
 package com.nfwork.dbfound.dto;
 
+import com.nfwork.dbfound.el.DBFoundEL;
 import java.util.List;
 
 public class QueryResponseObject<T> extends ResponseObject{
@@ -22,6 +23,72 @@ public class QueryResponseObject<T> extends ResponseObject{
 
 	public void setTotalCounts(long totalCounts) {
 		this.totalCounts = totalCounts;
+	}
+
+	public T get(){
+		if(datas == null || datas.size() == 0){
+			return null;
+		}
+		return datas.get(0);
+	}
+
+	public Object getProperty(String propertyName){
+		if(datas == null || datas.size() == 0){
+			return null;
+		}
+		return DBFoundEL.getData(propertyName,datas.get(0));
+	}
+
+	public String getString(String propertyName){
+		Object value = getProperty(propertyName);
+		if(value == null){
+			return null;
+		}
+		return value.toString();
+	}
+
+	public Integer getInt(String propertyName){
+		Object value = getProperty(propertyName);
+		if(value == null){
+			return null;
+		}
+		if(value instanceof Integer){
+			return (Integer)value;
+		}
+		return Integer.parseInt(value.toString());
+	}
+
+	public Long getLong(String propertyName){
+		Object value = getProperty(propertyName);
+		if(value == null){
+			return null;
+		}
+		if(value instanceof Long){
+			return (Long) value;
+		}
+		return Long.parseLong(value.toString());
+	}
+
+	public Double getDouble(String propertyName){
+		Object value = getProperty(propertyName);
+		if(value == null){
+			return null;
+		}
+		if(value instanceof Double){
+			return (Double) value;
+		}
+		return Double.parseDouble(value.toString());
+	}
+
+	public Float getFloat(String propertyName){
+		Object value = getProperty(propertyName);
+		if(value == null){
+			return null;
+		}
+		if(value instanceof Float){
+			return (Float) value;
+		}
+		return Float.parseFloat(value.toString());
 	}
 
 }
