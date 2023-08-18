9f03da6351866e580e8bd170239fc8d9e7f0a90f
huangjiong <huangjiong@gomo.com>
2023-08-18 17:39:17 +0800

    优化文件上传处理逻辑

--- src/main/java/com/nfwork/dbfound/web/file/FileUploadUtil.java
+++ src/main/java/com/nfwork/dbfound/web/file/FileUploadUtil.java
@@ -13,30 +13,31 @@ public class FileUploadUtil {
 
 	private static final ThreadLocal<List<DiskFileItem>> fileItemsLocal = new ThreadLocal<>();
 
+	public static boolean isUploadRequest(Context context) {
+		return JakartaServletFileUpload.isMultipartContent(context.request);
+	}
+
 	public static void initFileUpload(Context context) throws FileUploadException {
 
-		if (JakartaServletFileUpload.isMultipartContent(context.request)) { // 创建文件处理工厂，它用于生成
+		JakartaServletDiskFileUpload upload = new JakartaServletDiskFileUpload();
 
-			JakartaServletDiskFileUpload upload = new JakartaServletDiskFileUpload();
+		// 设置允许用户上传文件大小,单位:字节
+		upload.setSizeMax(1024L * 1024 * DBFoundConfig.getMaxUploadSize());
 
-			// 设置允许用户上传文件大小,单位:字节
-			upload.setSizeMax(1024L * 1024 * DBFoundConfig.getMaxUploadSize());
+		List<DiskFileItem> items = upload.parseRequest(context.request);
+		fileItemsLocal.set(items);
 
-			List<DiskFileItem> items = upload.parseRequest(context.request);
-			fileItemsLocal.set(items);
-
-			// 下面对每个字段进行处理，分普通字段和文件字段
-			for (DiskFileItem fileItem : items) {
-				String filedName = fileItem.getFieldName();
-				if (fileItem.isFormField()) {
-					context.setParamData(filedName, fileItem.getString());
-				} else {
-					context.setParamData(filedName, fileItem);
-					context.setParamData(filedName + "_name", fileItem.getName().substring(
-							fileItem.getName().lastIndexOf("\\") + 1));
-					context.setParamData(filedName + "_type", fileItem.getContentType());
-					context.setParamData(filedName + "_size", FileSizeCalculator.getFileSize(fileItem.getSize()));
-				}
+		// 下面对每个字段进行处理，分普通字段和文件字段
+		for (DiskFileItem fileItem : items) {
+			String filedName = fileItem.getFieldName();
+			if (fileItem.isFormField()) {
+				context.setParamData(filedName, fileItem.getString());
+			} else {
+				context.setParamData(filedName, fileItem);
+				context.setParamData(filedName + "_name", fileItem.getName().substring(
+						fileItem.getName().lastIndexOf("\\") + 1));
+				context.setParamData(filedName + "_type", fileItem.getContentType());
+				context.setParamData(filedName + "_size", FileSizeCalculator.getFileSize(fileItem.getSize()));
 			}
 		}
 	}
