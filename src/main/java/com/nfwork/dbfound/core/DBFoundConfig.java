f5eef9ddbe7c6dcede771b36c7e924a1c2b128c0
huangjiong <huangjiong@gomo.com>
2023-09-15 15:22:29 +0800

    release 3.3.12

--- src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
+++ src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
@@ -31,7 +31,7 @@ import com.nfwork.dbfound.web.i18n.MultiLangUtil;
 
 public class DBFoundConfig {
 
-	public static final String VERSION = "3.3.11" ;
+	public static final String VERSION = "3.3.12" ;
 
 	private static String listenerClass;
 	private final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
