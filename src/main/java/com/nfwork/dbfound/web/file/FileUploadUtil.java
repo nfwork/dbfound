b30f4cba4d3c0812c32ebb2c955c6821c5d2a87c
huangjiong <huangjiong@gomo.com>
2023-10-08 15:04:34 +0800

    优化代码

--- src/main/java/com/nfwork/dbfound/web/file/FileUploadUtil.java
+++ src/main/java/com/nfwork/dbfound/web/file/FileUploadUtil.java
@@ -1,49 +1,39 @@
 package com.nfwork.dbfound.web.file;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.nfwork.dbfound.core.DBFoundConfig;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import com.nfwork.dbfound.core.Context;
+import com.nfwork.dbfound.core.DBFoundConfig;
+import org.apache.commons.fileupload2.core.DiskFileItem;
+import org.apache.commons.fileupload2.javax.JavaxServletDiskFileUpload;
+import org.apache.commons.fileupload2.javax.JavaxServletFileUpload;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.nio.charset.Charset;
+import java.util.List;
 
 public class FileUploadUtil {
 
-	private static final ThreadLocal<List<FileItem>> fileItemsLocal = new ThreadLocal<>();
+	private static final ThreadLocal<List<DiskFileItem>> fileItemsLocal = new ThreadLocal<>();
 
 	public static boolean isUploadRequest(Context context) {
-		return ServletFileUpload.isMultipartContent(context.request);
+		return JavaxServletFileUpload.isMultipartContent(context.request);
 	}
 
-	public static void initFileUpload(Context context) throws IOException, FileUploadException {
-		// FileItem 对象。
-		DiskFileItemFactory factory = new DiskFileItemFactory(); // 设置文件的缓存路径
+	public static void initFileUpload(Context context) throws IOException {
 
-		factory.setSizeThreshold(5 * 1024 * 1024); // 设置最多只允许在内存中存储的数据,单位:字节
-
-		ServletFileUpload upload = new ServletFileUpload(factory);
-
-		String encoding = context.request.getCharacterEncoding();
-
-		upload.setHeaderEncoding(encoding);
+		JavaxServletDiskFileUpload upload = new JavaxServletDiskFileUpload();
 
 		// 设置允许用户上传文件大小,单位:字节
 		upload.setSizeMax(1024L * 1024 * DBFoundConfig.getMaxUploadSize());
 
-		List<FileItem> items ;
-
-		items = upload.parseRequest(context.request);
+		List<DiskFileItem> items = upload.parseRequest(context.request);
 		fileItemsLocal.set(items);
 
 		// 下面对每个字段进行处理，分普通字段和文件字段
-		for (FileItem fileItem : items) {
+		for (DiskFileItem fileItem : items) {
 			String filedName = fileItem.getFieldName();
 			if (fileItem.isFormField()) {
-				context.setParamData(filedName, fileItem.getString(encoding));
+				context.setParamData(filedName, fileItem.getString(Charset.forName(context.request.getCharacterEncoding())));
 			} else {
 				FilePart filePart = new CommonFilePart(fileItem);
 				Object object = context.getData("param."+filedName);
@@ -62,7 +52,7 @@ public class FileUploadUtil {
 	}
 
 	public static void clearFileItemLocal(){
-		List<FileItem> items  = fileItemsLocal.get();
+		List<DiskFileItem> items  = fileItemsLocal.get();
 		if(items != null){
 			items.forEach(item ->{
 				try { item.delete(); } catch (Exception ignore) {}
