d411d45a6be0c236d26d1419efd5f0343c48cbe0
huangjiong <huangjiong@gomo.com>
2023-11-07 10:06:19 +0800

    release 4.1.1

--- src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
+++ src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
@@ -28,7 +28,7 @@ import com.nfwork.dbfound.web.i18n.MultiLangUtil;
 
 public class DBFoundConfig {
 
-	public static final String VERSION = "4.1.0" ;
+	public static final String VERSION = "4.1.1" ;
 
 	private static String listenerClass;
 	private final static List<DataSourceConnectionProvide> dsp = new ArrayList<>();
