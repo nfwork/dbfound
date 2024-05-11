7b3ef30672454b3ff069ca39abd2c18008427ddc
huangjiong <huangjiong@gomo.com>
2024-05-11 15:45:24 +0800

    jakarta写法兼容处理

--- src/main/java/com/nfwork/dbfound/web/handler/ActionHandler.java
+++ src/main/java/com/nfwork/dbfound/web/handler/ActionHandler.java
@@ -9,8 +9,8 @@ import com.nfwork.dbfound.web.WebExceptionHandler;
 import com.nfwork.dbfound.web.WebWriter;
 import com.nfwork.dbfound.web.file.FileUploadUtil;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class ActionHandler {
 
