591af2540b8014111bbd9172d36eb30cfe3b2602
huangjiong <huangjiong@gomo.com>
2024-08-21 17:36:15 +0800

    优化代码

--- src/main/java/com/nfwork/dbfound/el/ELEngine.java
+++ src/main/java/com/nfwork/dbfound/el/ELEngine.java
@@ -10,10 +10,7 @@ public class ELEngine {
 	public static final String requestScope = "request.";
 	public static final String paramScope = "param.";
 	public static final String outParamScope = "outParam.";
-	public static final String cookieScope = "cookie.";
-	public static final String headerScope = "header.";
-
-	private static final Set<String> absolutePathSet = CollectionUtil.asSet(sessionScope,requestScope,paramScope,outParamScope,cookieScope,headerScope);
+	private static final Set<String> absolutePathSet = CollectionUtil.asSet("session","request","param","outParam","cookie","header");
 
 	public static boolean isAbsolutePath(String exeSourcePath){
 		if (DataUtil.isNull(exeSourcePath)){
@@ -23,9 +20,9 @@ public class ELEngine {
 		int index = exeSourcePath.indexOf(".");
 		String rootPath;
 		if(index > -1){
-			rootPath = exeSourcePath.substring(0,index+1);
+			rootPath = exeSourcePath.substring(0,index);
 		}else{
-			rootPath = exeSourcePath + ".";
+			rootPath = exeSourcePath;
 		}
 		return absolutePathSet.contains(rootPath) ;
 	}
