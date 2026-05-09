6381921e72c453c3e8c3a41f4f4f860400b85c96
huangjiong@gomo.com <huangjiong@gomo.com>
2026-05-09 22:14:38 +0800

    优化printContext代码

--- src/main/java/com/nfwork/dbfound/model/bean/PrintContext.java
+++ src/main/java/com/nfwork/dbfound/model/bean/PrintContext.java
@@ -1,6 +1,7 @@
 package com.nfwork.dbfound.model.bean;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.nfwork.dbfound.core.Context;
 import com.nfwork.dbfound.util.JsonUtil;
 import com.nfwork.dbfound.util.LogUtil;
@@ -9,11 +10,13 @@ import java.util.Map;
 
 public class PrintContext extends SqlEntity{
 
+    private static final ObjectWriter PRETTY_WRITER = JsonUtil.getObjectMapper().writerWithDefaultPrettyPrinter();
+
     @Override
     public void execute(Context context, Map<String, Param> params, String provideName) {
         try {
             LogUtil.info("Context infos:\ncurrentPath: "+context.getCurrentPath()+"\ndata: "
-                    + JsonUtil.getObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(context.getDatas()));
+                    + PRETTY_WRITER.writeValueAsString(context.getDatas()));
         } catch (JsonProcessingException e) {
             LogUtil.error("printContext failed",e);
         }
