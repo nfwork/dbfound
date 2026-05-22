747295f49a9623f6b9d346ee71992d9bb3bc7013
huangjiong@gomo.com <huangjiong@gomo.com>
2026-05-22 10:04:21 +0800

    release 4.4.3

--- src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
+++ src/main/java/com/nfwork/dbfound/core/DBFoundConfig.java
@@ -32,7 +32,7 @@ import com.nfwork.dbfound.web.i18n.MultiLangFacade;
 
 public class DBFoundConfig {
 
-	public static final String VERSION = "4.4.2" ;
+	public static final String VERSION = "4.4.3" ;
 	public static final String CLASSPATH = "${@classpath}";
 	public static final String PROJECT_ROOT = "${@projectRoot}";
 	private static final String JVM_PARAM_PREFIX = "dbfound.";
